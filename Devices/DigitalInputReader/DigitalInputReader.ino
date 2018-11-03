0f903aeff4c887e977231f955fb2eb2be3fc91eb
Nikita Tomilov <programmer174@icloud.com>
2018-11-03 21:37:07 +0300

    Devices: device for reading digital input

--- Devices/DigitalInputReader/DigitalInputReader.ino
+++ Devices/DigitalInputReader/DigitalInputReader.ino
@@ -1,8 +1,5 @@
 #include <SPI.h>
 #include <RF24Network.h>
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
 #include "RF24.h"
 #include "printf.h"
 
@@ -14,9 +11,7 @@ RF24 radio(8,9);                        // Set up nRF24L01 radio on SPI bus plus
 
 RF24Network network(radio);          // Network uses that radio
 
-const uint16_t this_node = 03;        // Address of our node in Octal format
-
-Adafruit_BMP280 bmp; // I2C
+const uint16_t this_node = 02;        // Address of our node in Octal format
 
 #define MSG_TYPE_GET_RQ 0
 #define MSG_TYPE_SET_RQ 1
@@ -24,20 +19,38 @@ Adafruit_BMP280 bmp; // I2C
 #define MSG_TYPE_SET_RP 3
 #define MSG_TYPE_PING 4
 #define MSG_TYPE_PONG 5
+#define MSG_TYPE_TRIGGER 6
 
-#define REG_TEMP 1
-#define REG_PRESS 2
+#define LED_PIN 5
+#define SENSOR_PIN 3
 
-#define MAX_REWRITE_ATTEMPTS 3
+#define REG_SENSOR 1
 
-#undef BMP280_ADDRESS 
-#define BMP280_ADDRESS  0x76
+#define MAX_REWRITE_ATTEMPTS 5
 
 typedef struct {
     uint8_t message_type;
     uint8_t reg_id;
     uint8_t payload[8];
 } nrf_message_t;
+
+void sendMsg(RF24NetworkHeader out_header, nrf_message_t out_msg) {
+  bool ok = false;
+  int attempts = 0;
+
+  while (!ok) {
+    ok = network.write(out_header, &out_msg, sizeof(out_msg));
+     if (ok)
+      Serial.println("ok.");
+    else
+      Serial.println("failed.");
+    attempts++;
+    if (attempts > MAX_REWRITE_ATTEMPTS) {
+      ok = true;
+      Serial.println("Failed too many times.");
+    }
+  }
+}
 
 void setup(void)
 {
@@ -48,38 +61,36 @@ void setup(void)
  
   SPI.begin();
   radio.begin();
-  radio.setPALevel(RF24_PA_MAX);
   printf_begin();
   radio.printDetails();
  
   network.begin(/*channel*/ 90, /*node address*/ this_node);
-  delay(2000);
 
-  if (!bmp.begin()) {  
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
-    while (1);
-  }
-
-  Serial.print(F("Temperature = "));
-  Serial.print(bmp.readTemperature());
-  Serial.println(" *C");
-  
-  Serial.print(F("Pressure = "));
-  Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
 }
 
-void floatToBytes(float y, uint8_t* bytes) {
-  uint32_t x = *((uint32_t*)&y);
-  bytes[0] = (x >> 24) & 0xff;
-  bytes[1] = (x >> 16) & 0xff;
-  bytes[2] = (x >> 8) & 0xff;
-  bytes[3] = (x >> 0) & 0xff;
-}
+bool triggered = false;
 
 void loop() {
+
+
+  digitalWrite(LED_PIN, digitalRead(SENSOR_PIN));
+
+  if (digitalRead(SENSOR_PIN) != 0) {
+    if (!triggered) {
+      RF24NetworkHeader out_header(0);
+      nrf_message_t out_msg;
+      out_msg.message_type = MSG_TYPE_TRIGGER;
+      out_msg.reg_id = REG_SENSOR;
+      out_msg.payload[0] = digitalRead(SENSOR_PIN);
+      sendMsg(out_header, out_msg);
+    }
+    triggered = true;
+  } else {
+    triggered = false;
+  }
   
-  network.update();
+  network.update();                         
+  
  
   if (network.available()) {
     RF24NetworkHeader in_header;
@@ -99,12 +110,10 @@ void loop() {
         Serial.print("Get rq on reg "); Serial.print(in_msg.reg_id); Serial.println();
         out_msg.message_type = MSG_TYPE_GET_RP;
         out_msg.reg_id = in_msg.reg_id;
-        if (in_msg.reg_id == REG_TEMP) {
-          float temp = bmp.readTemperature();
-          floatToBytes(temp, out_msg.payload);
-        } else if (in_msg.reg_id == REG_PRESS) {
-          float p = bmp.readPressure();
-          floatToBytes(p, out_msg.payload);
+        if (in_msg.reg_id == REG_SENSOR) {
+          out_msg.payload[0] = digitalRead(SENSOR_PIN);
+        } else {
+          out_msg.payload[0] = 0;
         }
         break;
       case MSG_TYPE_SET_RQ:
@@ -117,24 +126,12 @@ void loop() {
         Serial.println("Ping rq");
         out_msg.message_type = MSG_TYPE_PONG;
         out_msg.reg_id = in_msg.reg_id;
-        out_msg.payload[0] = in_msg.payload[0];
+        for (int i = 0; i < 8; i++) {
+          out_msg.payload[i] = in_msg.payload[i];
+        }
     }
 
-    bool ok = false;
-    int attempts = 0;
-
-    while (!ok) {
-      ok = network.write(out_header, &out_msg, sizeof(out_msg));
-       if (ok)
-        Serial.println("ok.");
-      else
-        Serial.println("failed.");
-      attempts++;
-      if (attempts > MAX_REWRITE_ATTEMPTS) {
-        ok = true;
-        Serial.println("Failed too many times.");
-      }
-    }
+    sendMsg(out_header, out_msg);
   }
   
 }
