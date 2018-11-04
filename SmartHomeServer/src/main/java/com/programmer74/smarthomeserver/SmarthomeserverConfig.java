3013966b71713883787a547dfb4ca5ec65cf7bd0
Nikita Tomilov <programmer174@icloud.com>
2018-11-04 16:59:16 +0300

    Server: pass gateway ip via command line argument

--- SmartHomeServer/src/main/java/com/programmer74/smarthomeserver/SmarthomeserverConfig.java
+++ SmartHomeServer/src/main/java/com/programmer74/smarthomeserver/SmarthomeserverConfig.java
@@ -2,6 +2,8 @@ package com.programmer74.smarthomeserver;
 
 import com.programmer74.smarthomeserver.communication.UDPGateway;
 import com.programmer74.smarthomeserver.messaging.MessagesGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.net.InetAddress;
@@ -9,10 +11,13 @@ import java.net.InetAddress;
 @Configuration
 public class SmarthomeserverConfig {
 
+  @Autowired
+  private ApplicationArguments applicationArguments;
+
   @Bean
   public UDPGateway udpCommunication() {
     try {
-      return new UDPGateway(InetAddress.getByName("192.168.0.101"), 1337);
+      return new UDPGateway(InetAddress.getByName(applicationArguments.getSourceArgs()[0]), 1337);
     } catch (Exception ex) {
       ex.printStackTrace();
       System.exit(-1);
